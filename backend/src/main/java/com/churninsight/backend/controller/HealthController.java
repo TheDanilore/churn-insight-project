e431ab4919a63ddfc0275b1b68c911a00c41b2f0
TheDanilore <arteagagordillol@gmail.com>
2025-12-17 18:31:48 -0500

    finished base structure

--- backend/src/main/java/com/churninsight/backend/controller/HealthController.java
+++ backend/src/main/java/com/churninsight/backend/controller/HealthController.java
@@ -14,7 +14,7 @@ public class HealthController {
     public Map<String, String> checkHealth() {
         return Map.of(
                 "status", "OK",
-                "message", "Backend Spring Boot corriendo exitosamente en Docker 🚀",
+                "message", "Backend Spring Boot corriendo exitosamente sin docker 🚀",
                 "version", "1.0.0");
     }
 }
