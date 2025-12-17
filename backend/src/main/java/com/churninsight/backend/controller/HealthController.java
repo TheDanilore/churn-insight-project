4f0c55c5f546e56aa7a66a7248ac04f287cd0a36
TheDanilore <arteagagordillol@gmail.com>
2025-12-17 17:55:43 -0500

    finished base structure

---
+++ backend/src/main/java/com/churninsight/backend/controller/HealthController.java
@@ -0,0 +1,20 @@
+package com.churninsight.backend.controller;
+
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/health")
+public class HealthController {
+
+    @GetMapping
+    public Map<String, String> checkHealth() {
+        return Map.of(
+                "status", "OK",
+                "message", "Backend Spring Boot corriendo exitosamente en Docker 🚀",
+                "version", "1.0.0");
+    }
+}
